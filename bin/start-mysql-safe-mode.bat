3948d2f191b117ba1c1b9a317d0dc450795991f6
Breith <b.breith@gmail.com>
2017-12-08 20:56:05 +0100

    Update start/stop mysql server

--- bin/start-mysql-safe-mode.bat
+++ bin/start-mysql-safe-mode.bat
@@ -5,7 +5,7 @@ QPROCESS "%prg%">NUL
 IF %errorlevel% GTR 0 (
 	echo "Process (%prg%) starting..."
 	cd "C:\server\mysql\bin"
-	start %prg% --safe-mode --skip-grant-tables --log-basename=C:\server\var\log\mysql\
+	%prg% --safe-mode --skip-grant-tables --log-basename=C:\server\var\log\mysql\
 ) else (
 	echo "Process (%prg%) already starting..."
 )
--- bin/start-mysql.bat
+++ bin/start-mysql.bat
@@ -8,7 +8,7 @@ QPROCESS "%prg%">NUL
 IF %errorlevel% GTR 0 (
 	echo "Process (%prg%) starting..."
 	cd "C:\server\mysql\bin"
-	start %prg% --defaults-file=C:\server\mysql\bin\my.ini --log-error=C:\server\var\log\mysql\
+	%prg% --defaults-file=C:\server\mysql\bin\my.ini --log-error=C:\server\var\log\mysql\
 ) else (
 	echo "Process (%prg%) already starting..."
 )
